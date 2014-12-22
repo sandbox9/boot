75465a78d9f88ac5eaba1b6b79fbf8d4161f8680
kyjin <yours012@gmail.com>
2014-12-22 13:43:50 +0900

    SSL Connector(Tomcat) 설정 추가

--- ari-boot/src/main/java/sandbox9/boot/ariboot/AriSiteApplication.java
+++ ari-boot/src/main/java/sandbox9/boot/ariboot/AriSiteApplication.java
@@ -1,16 +1,66 @@
 package sandbox9.boot.ariboot;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by chanwook on 2014. 12. 18..
  */
+@Configuration
 @ComponentScan(basePackages = {"sandbox9.boot.ariboot."})
 @EnableAutoConfiguration
 public class AriSiteApplication {
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(AriSiteApplication.class, args);
-    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AriSiteApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addAdditionalTomcatConnectors(createSslConnector());
+		tomcat.setPort(8080);
+		return tomcat;
+	}
+
+	private Connector createSslConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+		try {
+//      	TODO keysotre 위치 변경
+			File keystore = new ClassPathResource("blc-example.keystore").getFile();
+			File truststore = new ClassPathResource("blc-example.keystore").getFile();
+			connector.setScheme("https");
+			connector.setSecure(true);
+			connector.setPort(8443);
+			protocol.setSSLEnabled(true);
+			protocol.setKeystoreFile(keystore.getAbsolutePath());
+			protocol.setKeystorePass("broadleaf");
+			protocol.setTruststoreFile(truststore.getAbsolutePath());
+			protocol.setTruststorePass("broadleaf");
+			protocol.setKeyAlias("broadleaf");
+			return connector;
+		}
+		catch (IOException ex) {
+			throw new IllegalStateException("can't access keystore: [" + "keystore"
+					+ "] or truststore: [" + "keystore" + "]", ex);
+		}
+	}
 }
